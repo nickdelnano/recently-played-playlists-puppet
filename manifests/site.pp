7d2e80743a78e52fe9df57b345dcf3f1c35e5c21
ddelnano <ddelnano@gmail.com>
2019-02-17 02:00:06 +0000

    Refactor puppet-bundle to puppet_utils module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,8 @@
 node default {
-  $puppet_bundle_content = @(EOT)
-  #!/bin/bash
 
-  export GEM_HOME=vendor/gem
-  export GEM_PATH=$GEM_HOME:/opt/puppetlabs/puppet/lib/ruby/gems/2.4.0:$GEM_PATH
-  export PATH=/opt/puppetlabs/puppet/bin:$PATH
-  
-  bundle $*
-  | EOT
-
-  file { '/usr/local/bin/puppet-bundle':
-    content => $puppet_bundle_content,
-    owner   => 'root',
-    mode    => '0744',
-  }
-    include recently_played_playlists
-    include recently_played_playlists_parser
+  include puppet_utils
+  include recently_played_playlists
+  include recently_played_playlists_parser
 
   # This username needs to be deployed in .env of recently-played-playlists.
   $mysql_user = 'playlist_user'
